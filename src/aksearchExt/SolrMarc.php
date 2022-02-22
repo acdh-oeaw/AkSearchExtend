ea59ec818ec624bb5ddba95c9bfe90070f94a085
Mateusz Żółtak <zozlak@zozlak.org>
2022-02-22 17:40:27 +0100

    SolrMarc::getURLs(): implementation with labels

--- src/aksearchExt/SolrMarc.php
+++ src/aksearchExt/SolrMarc.php
@@ -29,7 +29,43 @@ class SolrMarc extends \AkSearch\RecordDriver\SolrMarc {
      * https://redmine.acdh.oeaw.ac.at/issues/19527
      */
     public function getURLs() {
-        return \Vufind\RecordDriver\SolrDefault::getURLs();
+        $retVal = [];
+
+        // Which fields/subfields should we check for URLs?
+        $fieldsToCheck = [
+            '856' => ['y', 'z', '3'], // Standard URL
+            '555' => ['a']         // Cumulative index/finding aids
+        ];
+
+        foreach ($fieldsToCheck as $field => $subfields) {
+            $urls = $this->getMarcRecord()->getFields($field);
+            if ($urls) {
+                foreach ($urls as $url) {
+                    // Is there an address in the current field?
+                    $address = $url->getSubfield('u');
+                    if ($address) {
+                        $address = $address->getData();
+
+                        // Is there a description?  If not, just use the URL itself.
+                        foreach ($subfields as $current) {
+                            $desc = $url->getSubfield($current);
+                            if ($desc) {
+                                break;
+                            }
+                        }
+                        if ($desc) {
+                            $desc = $desc->getData();
+                        } else {
+                            $desc = $address;
+                        }
+
+                        $retVal[] = ['url' => $address, 'desc' => $desc];
+                    }
+                }
+            }
+        }
+
+        return $retVal;
     }
 
     /**
