fe802eadda6e1571b43c86b1e0fd85e71e15b456
Mateusz Żółtak <zozlak@zozlak.org>
2022-03-30 16:17:16 +0200

    License headers set to MIT

--- src/aksearchExt/Module.php
+++ src/aksearchExt/Module.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Austrian Centre for Digital Humanities.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace aksearchExt;
 
 class Module {
@@ -13,10 +37,10 @@ class Module {
             'vufind' => [
                 'plugin_managers' => [
                     'recorddriver' => [
-                        'factories' => [
+                        'factories'  => [
                             'aksearchExt\SolrMarc' => 'VuFind\RecordDriver\SolrDefaultFactory'
                         ],
-                        'aliases' => [
+                        'aliases'    => [
                             'VuFind\RecordDriver\SolrMarc' => 'aksearchExt\SolrMarc'
                         ],
                         'delegators' => [
@@ -25,19 +49,19 @@ class Module {
                             ]
                         ]
                     ],
-                    'ils_driver' => [
+                    'ils_driver'   => [
                         'factories' => [
                             'aksearchExt\Alma' => 'VuFind\ILS\Driver\AlmaFactory'
                         ],
-                        'aliases' => [
+                        'aliases'   => [
                             'VuFind\ILS\Driver\Alma' => 'aksearchExt\Alma'
                         ]
                     ],
-                    'recordtab' => [
+                    'recordtab'    => [
                         'factories' => [
                             'aksearchExt\RecordTab\Exemplarspezifika' => 'VuFind\RecordTab\HoldingsILSFactory',
                         ],
-                        'aliases' => [
+                        'aliases'   => [
                             'exemplarspezifika' => 'aksearchExt\RecordTab\Exemplarspezifika'
                         ]
                     ],
@@ -45,5 +69,4 @@ class Module {
             ],
         ];
     }
-
 }
--- src/aksearchExt/SolrMarc.php
+++ src/aksearchExt/SolrMarc.php
@@ -1,5 +1,29 @@
 <?php
 
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Austrian Centre for Digital Humanities.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace aksearchExt;
 
 use File_MARC_Data_Field;
@@ -133,7 +157,8 @@ class SolrMarc extends \AkSearch\RecordDriver\SolrMarc {
                     $items            = $holding['items'];
                     $holding['items'] = [];
                     foreach ($items as $item) {
-                        $itemValues = [$item['barcode'], $item['enumeration_a'], $item['enumeration_b']];
+                        $itemValues = [$item['barcode'], $item['enumeration_a'],
+                            $item['enumeration_b']];
                         if (count(array_intersect($matchValue, $itemValues)) > 0) {
                             //print_r($item);
                             $holding['items'][] = $item;
