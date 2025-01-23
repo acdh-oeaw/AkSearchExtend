169a3bed74766063ea04b429fec4066b8202c76f
Mateusz Żółtak <zozlak@zozlak.org>
2025-01-23 10:03:46 +0100

    HoldingData: implement the \ArrayAccess interface
    (closes #9)

--- src/aksearchExt/container/HoldingData.php
+++ src/aksearchExt/container/HoldingData.php
@@ -33,7 +33,7 @@ use SimpleXMLElement;
  *
  * @author zozlak
  */
-class HoldingData {
+class HoldingData implements \ArrayAccess {
 
     public string $libraryCode       = '';
     public string $locationCode      = '';
@@ -118,4 +118,21 @@ class HoldingData {
         }
         return $data;
     }
+
+    public function offsetExists(mixed $offset): bool {
+        return isset($this->offset);
+    }
+
+    public function offsetGet(mixed $offset): mixed {
+        return $this->$offset;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void {
+        $this->$offset = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void {
+        unset($this->$offset);
+    }
 }
+
